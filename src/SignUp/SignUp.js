d74025693d8ed7890687b9a9f1fe7b7d8da3ebbd
gops12345 <agrawalgopala@gmail.com>
2019-07-24 15:20:14 +0530

    First Commit

---
+++ src/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component{
+    render(){
+        return(
+            <div>Sign Up At  the Page</div>
+        );
+    }
+}
+
+export default SignUp;
